afe605ba6fcaee6632f1123a0833576c115c8077
LucasTavara <lulucas@hotmail.com>
2024-10-28 15:00:45 -0300

    configure IDE

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret_key"
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
 
 db = SQLAlchemy(app)
