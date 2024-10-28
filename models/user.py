736924462aa8cf62b0ae3fb8a880e01d61e2687a
LucasTavara <lulucas@hotmail.com>
2024-10-28 17:18:26 -0300

    feat: Implementing Authentication & Crud Users

--- models/user.py
+++ models/user.py
@@ -1,8 +1,8 @@
-from enum import unique
 
-from app import db
+from Database import db
+from flask_login import UserMixin
 
-class User(db.Model):
+class User(db.Model,UserMixin):
     #mapeamento de informações
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80),nullable=False, unique=True)
